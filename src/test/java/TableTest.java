bcbacc6f33b9200b17a60b5ad34ec89b7fc8a1df
VovanOzz <120048518+Vovanozz@users.noreply.github.com>
2023-03-01 18:12:50 +0200

    HW_7

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -24,25 +24,28 @@ public class TableTest {
 
     @Test
     public void testTableCss(){
-        List<WebElement> elementsTR = wd.findElements(By.cssSelector("#customers tr"));
-        System.out.println("amount of table rows is:"+elementsTR.size());
-        List<WebElement> tableColumns = wd.findElements(By.cssSelector("#customers tr th"));
-        System.out.println("table columns is:"+tableColumns.size());
-        WebElement elementRow3 = wd.findElement(By.cssSelector("#customers tr:nth-child(3)"));
-        System.out.println(elementRow3.getText());
-        WebElement elementLastColumpRow3 = wd.findElement(By.cssSelector("#customers tr:nth-child(3) td:last-child"));
-        System.out.println(elementLastColumpRow3.getText());
-
-
-
-
-
+//        List<WebElement> elementsTR = wd.findElements(By.cssSelector("#customers tr"));
+//        System.out.println("amount of table rows is:"+elementsTR.size());
+//        List<WebElement> tableColumns = wd.findElements(By.cssSelector("#customers tr th"));
+//        System.out.println("table columns is:"+tableColumns.size());
+//        WebElement elementRow3 = wd.findElement(By.cssSelector("#customers tr:nth-child(3)"));
+//        System.out.println(elementRow3.getText());
+//        WebElement elementLastColumpRow3 = wd.findElement(By.cssSelector("#customers tr:nth-child(3) td:last-child"));
+//        System.out.println(elementLastColumpRow3.getText());
+        List<WebElement> elementsByXpathTR = wd.findElements(By.xpath("//table[@id='customers']/tbody/tr"));
+        System.out.println(elementsByXpathTR.size());
+        List<WebElement> elementsByXpathTH = wd.findElements(By.xpath("//table[@id='customers']/tbody/tr/th"));
+        System.out.println(elementsByXpathTH.size());
+        WebElement elementRow3ByXpath = wd.findElement(By.xpath("(//tr)[3]"));
+        System.out.println(elementRow3ByXpath.getText());
+        WebElement elementLastColumpByXpath = wd.findElement(By.xpath("//tr/th[3]"));
+        System.out.println(elementLastColumpByXpath.getText());
 
 
     }
     @AfterMethod
     public void postCondition(){
 
-       // wd.quit();
+       wd.quit();
     }
 }
